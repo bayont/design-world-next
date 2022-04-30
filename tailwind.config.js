ba327fd21182842a439c0b1bd4560d8c34791817
bayont <bayontmen@gmail.com>
2022-04-30 21:38:18 +0200

    feat: add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,35 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+   theme: {
+      extend: {
+         fontFamily: {
+            body: ['Inter', 'sans-serif'],
+         },
+         colors: {
+            'neutral-10': '#ffffff',
+            'neutral-20': '#F5F5F5',
+            'neutral-30': '#EDEDED',
+            'neutral-40': '#E0E0E0',
+            'neutral-50': '#C2C2C2',
+            'neutral-60': '#9E9E9E',
+            'neutral-70': '#757575',
+            'neutral-80': '#616161',
+            'neutral-90': '#404040',
+            'neutral-99': '#0A0A0A',
+            'primary-border': '#DFE0F3',
+            'primary-focus': 'rgba(76, 77, 220, 0.2)',
+            'primary-hover': '#3234CC',
+            'primary-main': '#4C4DDC',
+            'primary-pressed': '#21217A',
+            'primary-surface': '#F5F5FF',
+            'secondary-border': '#F3DFDF',
+            'secondary-focus': 'rgba(220, 76, 76, 0.2)',
+            'secondary-hover': '#CC3232',
+            'secondary-main': '#DC4C4C',
+            'secondary-pressed': '#7A2121',
+            'secondary-surface': '#FFF5F5',
+         },
+      },
+   },
+   plugins: [],
+};
