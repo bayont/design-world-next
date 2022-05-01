be2efd3ac1bdac5d7ef916bbbfd2d1b7f335cbc1
bayont <bayontmen@gmail.com>
2022-05-01 21:14:32 +0200

    feat: add signout page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,16 @@
 export const Footer = () => {
-   return <footer>Fabian Fetter &copy; 2022</footer>;
+   return (
+      <footer className="m-32">
+         <p>Fabian Fetter &copy; 2022</p>
+         <p>
+            UI designed by{' '}
+            <a
+               href="https://www.figma.com/community/file/1094887929807854794"
+               target="_blank"
+            >
+               Robbi Darwis
+            </a>
+         </p>
+      </footer>
+   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,27 @@
 import type { NextPage } from 'next';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 
 const Home: NextPage = () => {
+   const { data: session } = useSession();
    return (
       <>
-         <Link href="/login">
-            <button>Login</button>
-         </Link>
-         <Link href="/register">
-            <button>Register</button>
-         </Link>
+         {session ? (
+            <>
+               <Link href="/signout">
+                  <button>Sign out</button>
+               </Link>
+            </>
+         ) : (
+            <>
+               <Link href="/login">
+                  <button>Login</button>
+               </Link>
+               <Link href="/register">
+                  <button>Register</button>
+               </Link>
+            </>
+         )}
       </>
    );
 };
